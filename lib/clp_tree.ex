16809ce640e56ebcabaa02889df3fad3a1845024
Kat Lyons <lyons@users.noreply.github.com>
2014-09-18 00:55:04 -0600

    Make CLP.Tree use macros consistent with CLP.FD, better organise clp_tree.ex

--- lib/clp_tree.ex
+++ lib/clp_tree.ex
@@ -1,7 +1,6 @@
 defmodule MiniKanren.CLP.Tree do
   @moduledoc """
-  Provides the tree disequality operator `neq`, and hooks for using CLP(Tree)
-  in miniKanren.
+  Provides the tree disequality operator `neq`, and hooks for using CLP(Tree) in miniKanren.
   
   `use MiniKanren.CLP.Tree` to import the disequality operator.
   `use MiniKanren.CLP.Tree, :hooks` to set the hooks.
@@ -15,6 +14,12 @@ defmodule MiniKanren.CLP.Tree do
       import MiniKanren.CLP.Tree, only: [neq: 2]
     end
   end
+  defmacro __using__(:no_hooks) do
+    quote do
+      import MiniKanren.CLP.Tree, only: [neq: 2]
+      import MiniKanren.CLP.Tree.Functions
+    end
+  end
   defmacro __using__(:hooks) do
     quote do
       Process.put(:process_log, &MiniKanren.CLP.Tree.process_log/2)
@@ -26,33 +31,13 @@ defmodule MiniKanren.CLP.Tree do
     quote do
       import MiniKanren.CLP.Tree, only: [neq: 2]
       import MiniKanren.CLP.Tree.Functions
+      Process.put(:process_log, &MiniKanren.CLP.Tree.process_log/2)
+      Process.put(:enforce_constraints, &MiniKanren.CLP.Tree.enforce_constraints/1)
+      Process.put(:reify_constraints, &MiniKanren.CLP.Tree.reify_constraints/2)
     end
   end
   
-  @spec process_log(MK.unification_log, MK.constraint_store) :: MK.goal
-  def process_log(log, cons) do
-    recover_vars(log)
-    |> run_constraints(cons)
-  end
-  
-  @spec enforce_constraints(any) :: MK.goal
-  def enforce_constraints(_), do: &MK.unit/1
-  
-  @spec reify_constraints(MK.logic_value, MK.list_substitution) :: any
-  def reify_constraints(result_term, reified_names) do
-    fn _pkg = {_subs, cons, _doms, _counter} ->
-      # Reify variable names in constraint operands
-      reified_constraints = Enum.map(cons, fn c -> constraint_operands(c)
-                                                   |> walk_all(reified_names) end)
-      # Any constraint with an unreified variable is irrelevant
-      meaningful_constraints = Enum.reject(reified_constraints, &any_var?/1)
-      case meaningful_constraints do
-        [] -> result_term
-        _  -> [result_term, :- | {:neq, meaningful_constraints}]
-      end
-    end
-  end
-  
+# Operators -----------------------------------------------------------------------------------------
   @spec neq(MK.logic_value, MK.logic_value) :: MK.goal
   @doc """
   `neq` ensures two logic terms will never unify.
@@ -97,6 +82,32 @@ defmodule MiniKanren.CLP.Tree do
     end
   end
   
+# CLP Hooks -----------------------------------------------------------------------------------------
+  @spec process_log(MK.unification_log, MK.constraint_store) :: MK.goal
+  def process_log(log, cons) do
+    recover_vars(log)
+    |> run_constraints(cons)
+  end
+  
+  @spec enforce_constraints(any) :: MK.goal
+  def enforce_constraints(_), do: &MK.unit/1
+  
+  @spec reify_constraints(MK.logic_value, MK.list_substitution) :: any
+  def reify_constraints(result_term, reified_names) do
+    fn _pkg = {_subs, cons, _doms, _counter} ->
+      # Reify variable names in constraint operands
+      reified_constraints = Enum.map(cons, fn c -> constraint_operands(c)
+                                                   |> walk_all(reified_names) end)
+      # Any constraint with an unreified variable is irrelevant
+      meaningful_constraints = Enum.reject(reified_constraints, &any_var?/1)
+      case meaningful_constraints do
+        [] -> result_term
+        _  -> [result_term, :- | {:neq, meaningful_constraints}]
+      end
+    end
+  end
+  
+# Wiring --------------------------------------------------------------------------------------------
   @spec normalise_store(MK.unification_log, MK.package) :: MK.package
   def normalise_store(log, pkg = {_, cons, _, _}) do
     normalise_store_loop(cons, [], pkg, log)
